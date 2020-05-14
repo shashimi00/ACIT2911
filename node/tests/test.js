3ee4b81a5dd0c00bf5ddc13c87445a05f52667c5
shashimi00 <60232711+shashimi00@users.noreply.github.com>
2020-05-14 00:15:18 -0700

    Update test.js
    fixed // travis ci stuff

--- node/tests/test.js
+++ node/tests/test.js
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 chai.should();
 
 // travis ci stuff
-var url = "mongodb://localhost:27017/madmenDB";
+var url = "mongodb://localhost:27017/mydb_test";
 
 mongoose.createConnection(url, function(err, db) {
     if (err) throw err;
